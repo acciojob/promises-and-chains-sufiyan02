72d2c77d014bd7ce285a3b4035a9a7d08f326f45
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-03-06 10:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
   btn.addEventListener("click", function (event) {
     event.preventDefault(); 
 
-    const name = document.getElementById("name").value.trim();
-    const age = document.getElementById("age").value.trim();
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
 
     if (name === "" || age === "") {
       alert("Please enter valid details.");
@@ -19,17 +19,17 @@ document.addEventListener("DOMContentLoaded", function () {
     new Promise((resolve, reject) => {
       setTimeout(() => {
         if (ageNumber > 18) {
-          resolve(`Welcome, ${name}. You can vote.`);
+          resolve(`Welcome,. You can vote.`);
         } else {
-          reject(`Oh sorry ${name}. You aren't old enough.`);
+          reject(`Oh sorry. You aren't old enough.`);
         }
       }, 4000); 
     })
       .then((message) => {
         alert(message);
       })
-      .catch((errorMessage) => {
-        alert(errorMessage);
+      .catch((err) => {
+        alert(err);
       });
   });
 });
